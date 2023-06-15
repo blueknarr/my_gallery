ef66127cbb07b5b5677df91bf04dbbca614b58c4
blueknarr <knarrblue@gmail.com>
2023-06-15 22:08:35 +0900

    088. Image Picker로 사진 선택

---
+++ android/app/src/main/kotlin/com/sessac/my_gallery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sessac.my_gallery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyGalleryApp(),
+    );
+  }
+}
+
+class MyGalleryApp extends StatefulWidget {
+  const MyGalleryApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyGalleryApp> createState() => _MyGalleryAppState();
+}
+
+class _MyGalleryAppState extends State<MyGalleryApp> {
+  /// ImagePicker에서 사진을 여러 장 가져올 수 있다.
+  final ImagePicker _picker = ImagePicker();
+
+  /// 여러장의 사진은 Xfile형태이고 List로 받는데 처음에는 없기때문에 ?(Null) 허용한다.
+  List<XFile>? images;
+
+  /// 처음 사진을 선택할 수 있게 한다.
+  @override
+  void initState() {
+    super.initState();
+
+    loadImages();
+  }
+
+  /// 사진을 가져오는데 시간이 오래 걸린다. 그래서 Future로
+  Future<void> loadImages() async {
+    images = await _picker.pickMultiImage();
+
+    /// Future를 사용하는 images를 setState안에 넣으며 안된다.
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('전자액자'),
+        ),
+
+        /// 사진을 가져와서 메모리에 담았다가 사용한다.
+        body: Image.memory(bytes));
+  }
+}

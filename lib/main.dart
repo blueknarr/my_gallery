ef53cdfa84cc5f13603c57d475b302d6014264cc
blueknarr <knarrblue@gmail.com>
2023-06-15 22:54:45 +0900

    090. Timer, PageController로 사진 자동 전환하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -38,6 +39,9 @@ class _MyGalleryAppState extends State<MyGalleryApp> {
   /// 여러장의 사진은 Xfile형태이고 List로 받는데 처음에는 없기때문에 ?(Null) 허용한다.
   List<XFile>? images;
 
+  int currentPage = 0;
+  final pageConroller = PageController();
+
   /// 처음 사진을 선택할 수 있게 한다.
   @override
   void initState() {
@@ -49,6 +53,22 @@ class _MyGalleryAppState extends State<MyGalleryApp> {
   /// 사진을 가져오는데 시간이 오래 걸린다. 그래서 Future로
   Future<void> loadImages() async {
     images = await _picker.pickMultiImage();
+    if (images != null) {
+      Timer.periodic(const Duration(seconds: 5), (timer) {
+        currentPage++;
+
+        /// 사진 총 개수가 넘어가면 다시 처음으로 돌아가는 부분
+        if (currentPage > images!.length - 1) {
+          currentPage = 0;
+        }
+
+        pageConroller.animateToPage(
+          currentPage,
+          duration: Duration(microseconds: 500),
+          curve: Curves.easeIn,
+        );
+      });
+    }
 
     /// Future를 사용하는 images를 setState안에 넣으며 안된다.
     setState(() {});
@@ -65,6 +85,8 @@ class _MyGalleryAppState extends State<MyGalleryApp> {
       body: images == null
           ? const Center(child: Text('No data'))
           : PageView(
+              controller: pageConroller,
+
               /// PageView : 사진을 좌,우로 돌릴 수 있다.
               children: images!.map((image) {
                 return FutureBuilder<Uint8List>(
